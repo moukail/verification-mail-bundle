041af68f66525344bdd4b25caade08c4be8fd70e
Ismail Moukafih <i.moukafih@dbf.nl>
2021-12-14 09:46:30 +0100

    php 8.0

--- src/MoukailVerificationMailBundle.php
+++ src/MoukailVerificationMailBundle.php
@@ -3,11 +3,13 @@
 namespace Moukail\VerificationMailBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
 use Moukail\VerificationMailBundle\DependencyInjection\MoukailVerificationMailExtension;
 
 class MoukailVerificationMailBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new MoukailVerificationMailExtension();
